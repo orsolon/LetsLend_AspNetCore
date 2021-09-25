daec66e4c4adfd130981f0e9c7cb7e20920bb6e6
orsolon <56239735+orsolon@users.noreply.github.com>
2021-09-25 14:58:50 -0300

    create models

---
+++ LetsLend/Models/BorrowedItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetsLend.Models
+{
+    public class BorrowedItems
+    {
+        public BorrowedItems()
+        {
+            this.Itens = new List<Item>();
+        }
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int BorrowerId { get; set; }
+        public DateTime CatchDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public ICollection<Item> Itens { get; set; }
+    }
+}
---
+++ LetsLend/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsLend.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Description { get; set; }
+        public bool IsAvaiable { get; set; }
+    }
+}
